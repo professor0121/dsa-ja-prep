e76d489f02c4212076c02c305cb21ce791fbb187
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-08-05 11:41:26 +0530

    all day7 questions are done

--- day7/Palindromic.java
+++ day7/Palindromic.java
@@ -1,42 +1,38 @@
 import java.util.ArrayList;
+
 public class Palindromic {
     public static ArrayList<String> palindromicMaximumSubstrings(String s) {
         ArrayList<String> result = new ArrayList<>();
         int n = s.length();
-        int maxLength = 0;
+        int[] maxLen = {0}; 
 
         for (int i = 0; i < n; i++) {
-            int left = i, right = i;
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                if (right - left + 1 > maxLength) {
-                    maxLength = right - left + 1;
-                    result.clear();
-                    result.add(s.substring(left, right + 1));
-                } else if (right - left + 1 == maxLength) {
-                    result.add(s.substring(left, right + 1));
-                }
-                left--;
-                right++;
-            }
-
-            left = i;
-            right = i + 1;
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                if (right - left + 1 > maxLength) {
-                    maxLength = right - left + 1;
-                    result.clear();
-                    result.add(s.substring(left, right + 1));
-                } else if (right - left + 1 == maxLength) {
-                    result.add(s.substring(left, right + 1));
-                }
-                left--;
-                right++;
-            }
+            expandAroundCenter(s, i, i, result, maxLen);   
+            expandAroundCenter(s, i, i + 1, result, maxLen); 
         }
 
         return result;
     }
-    
+
+    private static void expandAroundCenter(String s, int left, int right, ArrayList<String> result, int[] maxLen) {
+        int n = s.length();
+        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            int currentLen = right - left + 1;
+            String palindrome = s.substring(left, right + 1);
+
+            if (currentLen > maxLen[0]) {
+                maxLen[0] = currentLen;
+                result.clear();
+                result.add(palindrome);
+            } else if (currentLen == maxLen[0]) {
+                result.add(palindrome);
+            }
+
+            left--;
+            right++;
+        }
+    }
+
     public static void main(String[] args) {
         String s = "babad";
         ArrayList<String> palindromes = palindromicMaximumSubstrings(s);
