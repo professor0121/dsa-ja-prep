0194f32e75f9a3c99614cc609586198c3407b9e3
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-08-22 12:04:23 +0530

    feat: Implement sorting algorithm for 0s, 1s, and 2s with example in main method

--- 08-22-2005/SortElement.java
+++ 08-22-2005/SortElement.java
@@ -1,4 +1,33 @@
 
 public class SortElement {
-    
+    public static void sort012(int arr[]) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    // Swap arr[low] and arr[mid]
+                    int temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    // Swap arr[mid] and arr[high]
+                    temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp;
+                    high--;
+                    break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={0,1,2,0,1,2};
+        sort012(arr);
+    }
 }
