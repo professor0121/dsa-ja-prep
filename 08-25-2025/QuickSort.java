a0f9772789b33bb0c34113783bcf5c6d8337052a
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-08-25 14:12:05 +0530

    feat: all changes made have a sancd

---
+++ 08-25-2025/QuickSort.java
@@ -0,0 +1,20 @@
+public class QuickSort  {
+    public static int Sort(int arr[],int low, int high){
+        System.out.println(low+""+high);
+        int pivot=arr[0];
+        for(int i=0; i<arr.length;i++){
+            for(int j=arr.length-1; j>=0; j--){
+                
+            }
+        }
+        for(int element:arr){
+            System.out.println(element);
+        }
+        return 0;
+    }
+    public static void main(String args[]){
+        int arr[]={7,8,3,2,5,4,1,6};
+        Sort(arr ,0,arr.length-1);
+        System.out.println("Hello abhishek");
+    }
+}
